a892f1e1af2f484b3a3911c3c7c8c4b02a16b704
TedDoser <TedDoser>
2016-11-16 02:14:41 +0000

    Changed text

--- joke.py
+++ joke.py
@@ -1,12 +1,14 @@
 '''This is a joke.'''
 joke1 = 'How many engineer does it take to change a router?'
 joke2 = 'Dumb blonde joke'
-all_jokes = True
+all_jokes = False
 
-#print (joke1)
+print (joke1)
 print ('This is a joke, folks.')
 
 if all_jokes :  
     print (joke1)
     print (joke2)
- 
+else : 
+    print (joke1)
+
