06444b2f637bcada5233d52d21c8d4efb337c071
TedDoser <TedDoser>
2016-11-16 02:07:16 +0000

    Update Jokes

--- joke.py
+++ joke.py
@@ -1,3 +1,12 @@
 '''This is a joke.'''
+joke1 = 'How many engineer does it take to change a router?'
+joke2 = 'Dumb blonde joke'
+all_jokes = True
 
+#print (joke1)
 print ('This is a joke, folks.')
+
+if all_jokes :  
+    print (joke1)
+    print (joke2)
+ 
